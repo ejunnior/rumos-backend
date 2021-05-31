97aedc68b92a61bccb85a69b4dd1f5d8af6d8303
Edvaldo Junior <ejunnior@gmail.comgit config --global user.email ejunnior@gmail.comgit config --global user.email ejunnior@gmail.com>
2021-05-31 19:45:34 +0100

    .

--- service/src/Sales.Application/ShoppingCart/RegisterProductHandler.cs
+++ service/src/Sales.Application/ShoppingCart/RegisterProductHandler.cs
@@ -1,6 +1,14 @@
 ﻿namespace Sales.Application.ShoppingCart
 {
+    using System.Threading.Tasks;
+    using Domain.ShoppingCart.Aggregates.ProductAggregate;
+
     public class RegisterProductHandler
     {
+        public RegisterProductHandler()
+        {
+        }
+
+        public async Task HandleAsync(RegisterProductCommand)
     }
 }
